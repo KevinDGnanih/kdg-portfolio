e3d7398eff9178a69bf42bc349b0c50aab451558
KevinDGnanih <kevin.gnanih@gmail.com>
2023-03-08 21:36:24 +0000

    Latest version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,9 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setactive] = useState('');
+
+
   return (
     <nav className={`
       ${styles.paddingX} w-full flex
@@ -13,7 +16,16 @@ const Navbar = () => {
       bg-primary`}>
         <div className='w-full flex justify-between
         items-center max-w-7xl mx-auto'>
-
+          <Link
+            to='/'
+            className='flex items-center gap-2'
+            onClick={() => {
+              setActive('');
+              window.scrollTo(0.0);
+            }}>
+              <img src={logo} alt='logo' className='w-9
+              h-9 object-contain' />
+          </Link>
         </div>
 
       </nav>
