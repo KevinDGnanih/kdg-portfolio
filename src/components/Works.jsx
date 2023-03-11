ca2e2234fa84ab7fa52ceaff0e566fe25c726150
KevinDGnanih <kevin.gnanih@gmail.com>
2023-03-11 20:57:05 +0000

    latest version

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -21,10 +21,11 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
       className='bg-tertiary p-5 rounded-2xl
       sm:w-[360px] w-full'>
         <div className='relative w-full h-[230px]'>
-        <img 
-        src={image}
-        alt={name}
-        className='w-full h-full object-cover rounded-2xl'/>
+          <img 
+            src={image}
+            alt={name}
+            className='w-full h-full object-cover rounded-2xl'
+          />
 
         <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
           <div
@@ -35,11 +36,9 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
                 src={github}
                 alt='github'
                 className='w-1/2 h-1/2 object-contain'/>
-
           </div>
-
         </div>
-        </div>
+      </div>
 
         <div className='mt-5'>
               <h3 className='text-white font-bold text-[24px]'>{name}</h3>
@@ -53,13 +52,11 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
             </p>))}
 
         </div>
-
       </Tilt>
-
     </motion.div>
-  )
+  );
 
-}
+};
 
 const Works = () => {
   return (
